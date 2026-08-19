8b013798292f65437aad1ed702c6cd10559a0f97
Humaira Akter Suchi <186734076+Suchi-A@users.noreply.github.com>
2026-08-19 20:16:00 +0600

    Add files via upload

---
+++ if_else.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+    int A;
+    int B;
+    scanf("%d",&A);
+    scanf("%d",&B);
+    if(A>=B)
+    {
+        printf("Yes");
+    }
+    else{
+        printf("No");
+    }
+    return 0;
+}
